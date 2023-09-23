d6b65f73344b8e8d63bb951100eb8c4e0ad39ff8
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-23 23:50:04 +0200

    Instal react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import AboutUs from "./AboutUs";
 import CatsList from "./CatsList";
